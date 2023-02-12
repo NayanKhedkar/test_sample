f1cdea8dcbc66072733970cd3da6d3e5fc43b141
NayanKhedkar <khedkarnayan08@gmail.com>
2023-02-12 14:18:19 +0530

    volume updates

--- custom.js
+++ custom.js
@@ -1,9 +1,12 @@
 (function () {
-
+    // check until Player object created
     var check = setInterval(function () {
-        if (GetPlayer) {
-            setDevice(GetPlayer());
+        if (typeof GetPlayer != 'undefined') {
             clearInterval(check);
+            setTimeout(function () {
+                onVolumeChange(GetPlayer());
+            }, 1000)
+            setDevice(GetPlayer());
         }
     }, 2000);
 
@@ -13,6 +16,31 @@
         console.log(isMobile.desktop);
         player.SetVar('isMobile', !isMobile.desktop);
     }
+
+    function onVolumeChange(player) {
+        document.querySelector('#volume button').addEventListener('click', function (e, val) {
+            var volume = document.querySelector('#volume .slider-bar input').value;
+            player.SetVar('volume', +volume);
+            window.postMessage("volume", "*");
+        });
+        document.querySelector('#volume .slider-bar input').addEventListener('change', function (e, val) {
+            var volume = e.target.value;
+            player.SetVar('volume', +volume);
+            window.postMessage("volume", "*");
+        });
+    }
+
+    
 })();
 
-//
+var updateVolume = function (volume) {
+    if (volume != undefined) {
+        var player = GetPlayer();
+        var volume = player.GetVar('volume') / 10;
+        try {
+            DS.appState.setVolume(volume);
+        } catch (error) {
+            console.log(error);
+        }
+    }
+};
